ed0305442bb752efb93722b651eef439561fc0a8
faisalmirji123 <faisalmirji@gmail.com>
2021-10-20 23:50:47 +0530

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import React from 'react'
 import { ChatEngine } from 'react-chat-engine';
+import ChatFeed from "./Components/ChatFeed"
+
 const App = () =>{
   return(
     <ChatEngine
@@ -8,6 +10,7 @@ const App = () =>{
     projectID="9d512b75-1783-4aa5-a8cd-4746e9972ca0"
     userName="faisal"
     userSecret="a2206872"
+    renderChatFeed={(chatAppProps)} =>   <ChatFeed {...chatAppProps  }
     />
   );
 }
