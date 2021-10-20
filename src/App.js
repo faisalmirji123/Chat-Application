4778e3f2ce3203f63c9852b2e3411029050e418c
faisalmirji123 <faisalmirji@gmail.com>
2021-10-20 09:06:27 +0530

    1 file modified, 1 added, 1 deleted

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from "react";
-import "./style.css";
-
-export default function App() {
-  return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+import './App.css';
+import { ChatEngine } from 'react-chat-engine';
+const App = () =>{
+  return(
+    <ChatEngine
+    height = "100vh"
+    projectID="9d512b75-1783-4aa5-a8cd-4746e9972ca0"
+    userName="faisal"
+    userSecret="a2206872"
+    />
   );
 }
+
+export default App;
